267c2340063a061a01601fee54a49e32bf88365e
Craig Thomas <craig@metail.co.uk>
2018-03-27 16:35:35 +0100

    Initial Stab at ILA2T this is specific to the metail need

---
+++ NModbus.Hardware.IntegrationTests/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+
+namespace NModbus.Hardware.IntegrationTests
+{
+  using System.Net.Sockets;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program.ModbusTcpMasterReadInputs();
+    }
+
+    public static void ModbusTcpMasterReadInputs()
+    {
+      using (TcpClient client = new TcpClient("192.168.100.10", 502))
+      {
+        var factory = new ModbusFactory();
+
+        IModbusMaster master = factory.CreateMaster(client);
+        var schniederMotion = new NModbus.Hardware.Schneider.Servo.ILA2T(master);
+
+        var pos = schniederMotion.Possition;
+        schniederMotion.Possition = 360;
+        pos = schniederMotion.Possition;
+
+        //var temp = master.ReadHoldingRegisters(1, 4612, 2);
+        //read five input values
+        var enhancedModbus = new NModbus.Extensions.ModbusMasterEnhanced(master, 32, NModbus.Extensions.Functions.Endian.LittleEndian);
+        var floats = enhancedModbus.ReadUshortHoldingRegisters(1, 4612, 1);
+
+
+
+        //Wild goose chase
+        //master.WriteMultipleRegisters(1, 6922, new ushort[] { 0, 0 });
+        //master.WriteMultipleRegisters(255, 6922, new ushort[] { 0, 2 });
+        //master.WriteMultipleRegisters(1, 6922, new ushort[] { 0, 0 });
+        //master.WriteMultipleRegisters(1, 6922, new ushort[] { 0, 0 });
+        //master.WriteMultipleRegisters(1, 6922, new ushort[] { 0, 0 });
+        //master.WriteMultipleRegisters(255, 6922, new ushort[] { 0, 1000 });
+
+        //master.WriteMultipleRegisters(255, 6922, new ushort[] { (512 + 3 + 128), 1000 });
+        //enhancedModbus.WriteUIntHoldingRegisters(255, 6922, new uint[] { 33554432 });
+
+        //enhancedModbus.WriteUshortHoldingRegisters(255, 6922, new ushort[] { 512 });
+        //enhancedModbus.WriteUshortHoldingRegisters(255, 6922, new ushort[] { 256 });
+        //enhancedModbus.WriteUshortHoldingRegisters(255, 6922, new ushort[] { 512 });
+        //enhancedModbus.WriteUshortHoldingRegisters(255, 6922, new ushort[] { (512 + 32 + 3), 1900 });
+
+
+        try
+        {
+          var accesslock = schniederMotion.ReadUshortHoldingRegisters(1, 282, 1)[0];
+          if (accesslock == 0)
+            schniederMotion.WriteUshortHoldingRegisters(1, 282, new ushort[] { 1 });
+          System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+          accesslock = schniederMotion.ReadUshortHoldingRegisters(1, 282, 1)[0];
+          var control = schniederMotion.ReadUshortHoldingRegisters(1, 6914, 1)[0];
+          if (accesslock == 1)
+
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 128 });
+          System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+          schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 6 });
+          System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+          control = schniederMotion.ReadUshortHoldingRegisters(1, 6914, 1)[0];
+          if (control == 6)
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+          System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+          control = schniederMotion.ReadUshortHoldingRegisters(1, 6914, 1)[0];
+          //if (control == 15)
+          //{
+          //  schniederMotion.WriteUshortHoldingRegisters(1, 6918, new ushort[] { 6 });
+          //  schniederMotion.WriteUshortHoldingRegisters(1, 6936, new ushort[] { 24 });
+          //  schniederMotion.WriteUshortHoldingRegisters(1, 10250, new ushort[] { 400 });
+          //  schniederMotion.WriteUshortHoldingRegisters(1, 10248, new ushort[] { 600 });
+
+          //  schniederMotion.WriteIntHoldingRegisters(1, 10252, new int[] { (360*200000) });
+          //  schniederMotion.WriteIntHoldingRegisters(1, 10262, new int[] { 0 });
+          //  schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+          //}
+          //schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+          var opmode = schniederMotion.ReadUshortHoldingRegisters(1, 6918, 1)[0];
+          
+          if (control == 15)
+            schniederMotion.WriteUshortHoldingRegisters(1, 6918, new ushort[] { 1 });
+          System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+          opmode = schniederMotion.ReadUshortHoldingRegisters(1, 6918, 1)[0];
+          if (opmode == 1)
+          {
+            schniederMotion.WriteUshortHoldingRegisters(1, 4612, new ushort[] { 3100 });
+            var timespan = TimeSpan.FromSeconds(5);
+            schniederMotion.Possition = 0;
+            ushort speed = 15;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 720;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 45;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 135;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 180;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 225;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 315;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Possition = 360;
+            schniederMotion.Speed = speed;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+            System.Threading.Thread.Sleep(timespan);
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+            schniederMotion.Speed = speed;
+            schniederMotion.Possition = 0;
+            schniederMotion.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 31 });
+
+
+
+          }
+          enhancedModbus.WriteUshortHoldingRegisters(1, 282, new ushort[] { 0 });
+
+
+        }
+        catch
+        {
+        }
+        //try
+        //{
+        //  enhancedModbus.WriteUshortHoldingRegisters(1, 6914, new ushort[] { 15 });
+        //}
+        //catch { }
+
+        //var operatingMode = schniederMotion.ControlMode;
+        //schniederMotion.WriteUIntHoldingRegisters(1,6914, new uint[] { 6 });
+        //schniederMotion.SwitchOn = true;
+        //schniederMotion.EnableVoltage = true;
+        //schniederMotion.QuickStop = true;
+        //schniederMotion.EnableOperation = true;
+
+        //operatingMode = schniederMotion.ControlMode;
+        //System.Console.WriteLine($"OpertaionMode: {operatingMode}");
+
+
+
+
+        //var encoder = enhancedModbus.ReadIntHoldingRegisters(1, 7706, 1);
+        //var newvalue = encoder[0] + 10;
+        //enhancedModbus.WriteIntHoldingRegisters(1,1324, new int[]{newvalue});
+
+      }
+    }
+  }
+}
---
+++ NModbus.Hardware/Schneider/Servo/ILA2T.cs
@@ -0,0 +1,143 @@
+﻿namespace NModbus.Hardware.Schneider.Servo
+{
+    using System;
+    using System.Collections;
+    using NModbus.Extensions.Functions;
+    public partial class ILA2T
+    {
+        private readonly IModbusMaster master;
+        private readonly uint wordSize;
+        private readonly Func<byte[], byte[]> endian;
+        private readonly bool wordSwap;
+        private readonly bool frontPadding;
+        public const int EncoderScalling = 200000;
+        public const ushort RpmScalling = 200;
+
+
+        public ILA2T(IModbusMaster master)
+        {
+            this.master = master;
+            this.wordSize = 32;
+            this.endian = Endian.LittleEndian;
+            this.wordSwap = true;
+            this.frontPadding = false;
+        }
+        #region Motor Functions
+
+
+        public ushort Speed
+        {
+            get
+              => (ushort)(this.ReadUshortHoldingRegisters(1, Map.TargetSpeedAddress, 1)[0] / ILA2T.RpmScalling);
+            set
+              => this.WriteIntHoldingRegisters(1, Map.TargetSpeedAddress, new[] { (int)(value * ILA2T.RpmScalling) });
+        }
+
+        public int Possition
+        {
+            get
+              => this.ReadIntHoldingRegisters(1, Map.TargetPossitionAddress, 1)[0] / ILA2T.EncoderScalling;
+            set
+              => this.WriteIntHoldingRegisters(1, Map.TargetPossitionAddress, new[] { (int)(value * ILA2T.EncoderScalling) });
+        }
+
+        public uint ControlMode
+        {
+            get
+              => this.ReadUintHoldingRegisters(1, Map.ControlModeAddress, 1)[0];
+            set
+              => this.WriteUIntHoldingRegisters(1, Map.ControlModeAddress, new[] { value });
+        }
+
+        public int OperatingMode
+        {
+            get
+              => this.ReadIntHoldingRegisters(1, Map.OperatingModeAddress, 1)[0];
+            set
+              => this.WriteIntHoldingRegisters(1, Map.OperatingModeAddress, new[] { value });
+        }
+
+        public static byte[] BitArrayToByteArray(BitArray bits)
+        {
+            byte[] ret = new byte[(bits.Length - 1) / 8 + 1];
+            bits.CopyTo(ret, 0);
+            return ret;
+        }
+
+
+        #endregion
+
+        #region Read/Write Registers
+
+        public ushort[] ReadUshortHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+            => RegisterFunctions.ByteValueArraysToUShorts(
+              RegisterFunctions.ReadRegisters(slaveAddress, startAddress, numberOfPoints, this.master, this.wordSize, this.endian, this.wordSwap), this.frontPadding);
+
+        public short[] ReadShortHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+          => RegisterFunctions.ByteValueArraysToShorts(
+            RegisterFunctions.ReadRegisters(slaveAddress, startAddress, numberOfPoints, this.master, this.wordSize, this.endian, this.wordSwap), this.frontPadding);
+
+        public uint[] ReadUintHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+          => RegisterFunctions.ByteValueArraysToUInts(
+            RegisterFunctions.ReadRegisters(slaveAddress, startAddress, numberOfPoints, this.master, this.wordSize, this.endian, this.wordSwap), this.frontPadding);
+
+        public int[] ReadIntHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+          => RegisterFunctions.ByteValueArraysToInts(
+            RegisterFunctions.ReadRegisters(slaveAddress, startAddress, numberOfPoints, this.master, this.wordSize, this.endian, this.wordSwap), this.frontPadding);
+
+        public float[] ReadFloatHoldingRegisters(byte slaveAddress, ushort startAddress, ushort numberOfPoints)
+          => RegisterFunctions.ByteValueArraysToFloats(
+               RegisterFunctions.ReadRegisters(slaveAddress, startAddress, numberOfPoints, this.master, this.wordSize, this.endian, this.wordSwap), this.frontPadding);
+
+        public void WriteUshortHoldingRegisters(byte slaveAddress, ushort startAddress, ushort[] data)
+          => RegisterFunctions.WriteRegistersFunc(
+            slaveAddress,
+            startAddress,
+            RegisterFunctions.UShortsToByteValueArrays(data, this.wordSize, this.frontPadding),
+            this.master,
+            this.wordSize,
+            this.endian,
+            this.wordSwap);
+
+        public void WriteShortHoldingRegisters(byte slaveAddress, ushort startAddress, short[] data)
+          => RegisterFunctions.WriteRegistersFunc(
+            slaveAddress,
+            startAddress,
+            RegisterFunctions.ShortsToByteValueArrays(data, this.wordSize, this.frontPadding),
+            this.master,
+            this.wordSize,
+            this.endian,
+            this.wordSwap);
+
+        public void WriteIntHoldingRegisters(byte slaveAddress, ushort startAddress, int[] data)
+          => RegisterFunctions.WriteRegistersFunc(
+            slaveAddress,
+            startAddress,
+            RegisterFunctions.IntToByteValueArrays(data, this.wordSize, this.frontPadding),
+            this.master,
+            this.wordSize,
+            this.endian,
+            this.wordSwap);
+
+        public void WriteUIntHoldingRegisters(byte slaveAddress, ushort startAddress, uint[] data)
+          => RegisterFunctions.WriteRegistersFunc(
+            slaveAddress,
+            startAddress,
+            RegisterFunctions.UIntToByteValueArrays(data, this.wordSize, this.frontPadding),
+            this.master,
+            this.wordSize,
+            this.endian,
+            this.wordSwap);
+
+        public void WriteFloatHoldingRegisters(byte slaveAddress, ushort startAddress, float[] data)
+          => RegisterFunctions.WriteRegistersFunc(
+            slaveAddress,
+            startAddress,
+            RegisterFunctions.FloatToByteValueArrays(data, this.wordSize, this.frontPadding),
+            this.master,
+            this.wordSize,
+            this.endian,
+            this.wordSwap);
+        #endregion
+    }
+}
